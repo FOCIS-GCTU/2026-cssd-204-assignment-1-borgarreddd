e84447608a15aa31147eae79a64689d0209da928
borgarreddd <2425403074@live.gctu.edu.gh>
2026-06-03 15:18:42 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,28 +1,28 @@
 
 def main():
-# file: initials .py
-# Descirption : print out my initials
-# Assignment Number 1
-#Name: LAWSON ARHIN DAVID
-#STUDENT ID:2425403074
-# Email: 2425403074@live.gctu.edu.gh
-# Grader:  AUGUSTUS buckman
-# On my  LAWSON ARHIN DAVID  This programming assignment is my own work
-# and i have not provided this code to any other student
-print()
-print("...LAD")
-print()
-print("...LLL.....................AAAA...............DDDDDDDD.......")
-print("...LLL.................AAA......AAA........DDD........DD.....")
-print("...LLL.................AAA......AAA........DDD........DDD....")
-print("...LLL.................AAA......AAA........DDD........DDD....")
-print("...LLL.................AAAAAAAAAAAA........DDD........DDD....")
-print("...LLL.................AAA......AAA........DDD........DDD....")
-print("...LLL.................AAA......AAA........DDD........DDD....")
-print("...LLL.................AAA......AAA........DDD........DD.....")
-print("...LLL............**...AAA......AAA...**...DDD........DD...**")
-print("...LLLLLLLLLLLL...**...AAA......AAA...**...DDDDDDDDDDDD....**")
-print()
+  # file: initials .py
+  # Descirption : print out my initials
+  # Assignment Number 1
+  #Name: LAWSON ARHIN DAVID
+  #STUDENT ID:2425403074
+  # Email: 2425403074@live.gctu.edu.gh
+  # Grader:  AUGUSTUS buckman
+  # On my  LAWSON ARHIN DAVID  This programming assignment is my own work
+  # and i have not provided this code to any other student
+  print()
+  print("...LAD")
+  print()
+  print("...LLL.....................AAAA...............DDDDDDDD.......")
+  print("...LLL.................AAA......AAA........DDD........DD.....")
+  print("...LLL.................AAA......AAA........DDD........DDD....")
+  print("...LLL.................AAA......AAA........DDD........DDD....")
+  print("...LLL.................AAAAAAAAAAAA........DDD........DDD....")
+  print("...LLL.................AAA......AAA........DDD........DDD....")
+  print("...LLL.................AAA......AAA........DDD........DDD....")
+  print("...LLL.................AAA......AAA........DDD........DD.....")
+  print("...LLL............**...AAA......AAA...**...DDD........DD...**")
+  print("...LLLLLLLLLLLL...**...AAA......AAA...**...DDDDDDDDDDDD....**")
+  print()
 
 
 
